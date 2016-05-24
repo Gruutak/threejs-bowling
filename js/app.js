2105f093293b131be72fc6077e69023545881462
bertZ <lucasbertz@hotmail.com>
2016-05-24 16:31:25 +0000

    Movimento da bola

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+//criando a cena
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.lookAt(new THREE.Vector3(0,0,0));
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,9 +18,32 @@ camera.position.z=50;
 
 function render() {
 	requestAnimationFrame( render );
+	bola.rotation.y+=0.5;
 	var time = Date.now() * 0.001;
-	bola.rotation.x+=0.1;
-	bola.rotation.y+=0.1;
 	renderer.render( scene, camera );
 }
 render();
+
+
+//Controles
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37:
+        	bola.position.x+=-0.5;;
+        break;
+
+        case 38: // up
+        break;
+
+        case 39:
+        	bola.position.x+=0.5;
+        break;
+
+        case 40: // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
