25ab109e83c08cd3d8ae0988b52d91d907099460
bertZ <lucasbertz@hotmail.com>
2016-05-29 20:11:00 +0000

    Pinos adicionados

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $("body").addClass("loading");
 
 //Variaveis locais
 var scene, camera, renderer; //Elementos basicos para funcionamento
-var bola, pista; //Objetos
+var bola, pista, pinos = new Array(10); //Objetos
 var debug = false;
 var jsonLoader = new THREE.JSONLoader();
 var textureLoader = new THREE.TextureLoader();
@@ -26,7 +26,7 @@ function init() {
 
 	renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setSize(WIDTH, HEIGHT);
-	renderer.setClearColor( 0xffffff );
+	renderer.setClearColor( 0x000000 );
 
 	document.body.appendChild(renderer.domElement);
 
@@ -39,16 +39,7 @@ function init() {
 	//camera.lookAt(new THREE.Vector3(0,0,0));
 	scene.add(camera);
 
-	//Função para caso a janela seja redimensionada
-	window.addEventListener('resize', function() {
-	    var WIDTH = window.innerWidth-60,
-	        HEIGHT = window.innerHeight;
-    	renderer.setSize(WIDTH, HEIGHT);
-    	camera.aspect = WIDTH / HEIGHT;
-    	camera.updateProjectionMatrix();
-    });
-
-    //Iluminação
+	//Iluminação
     scene.add( new THREE.AmbientLight( 0xffffff ) );
 
     //criando a bola
@@ -76,38 +67,126 @@ function init() {
 		scene.add(pista);
 	});
 
+	//pinos
+	jsonLoader.load( "js/models/bowling-pin.json", function ( geometry, materials ) {
+		var material = materials[1];
+		material.morphTargets = true;
+		material.color.setHex( 0xff0000 );
+
+		var faceMaterial = new THREE.MultiMaterial( materials );
+
+		for( var i = 0; i < 10; i++){
+			pinos[i] = new THREE.Mesh( geometry, faceMaterial );
+			pinos[i].scale.set( 15, 15, 15);
+
+			pinos[i].position.y = 41;
+
+			if(i > 5) {
+				pinos[i].position.z = -480;
+
+				switch(i){
+					case 6:
+						pinos[i].position.x = -60
+						break;
+
+					case 7:
+						pinos[i].position.x = -20
+						break;
+
+					case 8:
+						pinos[i].position.x = 20
+						break;
+
+					case 9:
+						pinos[i].position.x = 60
+						break;
+
+				}
+			}
+			else if(i > 2) {
+				pinos[i].position.z = -450;
+
+				switch(i){
+					case 3:
+						pinos[i].position.x = -40
+						break;
+
+					case 4:
+						pinos[i].position.x = 0
+						break;
+
+					case 5:
+						pinos[i].position.x = 40
+						break;
+
+				}
+			}
+			else if(i > 0) {
+				pinos[i].position.z = -420;
+
+				switch(i){
+					case 1:
+						pinos[i].position.x = -20
+						break;
+
+					case 2:
+						pinos[i].position.x = 20
+						break;
+				}
+			}
+			else
+				pinos[i].position.z = -390;
+
+
+			scene.add(pinos[i]);
+		}
+	});
+
+
 	//Controle de orbita com o mouse para
 	if(window.location.hash == '#debug') {
 		debug = true;
   		orbitCcontrols = new THREE.OrbitControls(camera, renderer.domElement);
 	}
 
-
 	//Controles
 	$(document).keydown(function(e) {
-	    switch(e.which) {
-	        case 37:
-	        	bola.rotation.y-=0.1;
-	        	bola.position.x+=-0.5;
-	        	bola.rotation.y+=0;
-	        break;
+		    switch(e.which) {
+		        case 37:
+		        	if(bola.position.x > -125) {
+			        	bola.rotation.y-=0.1;
+			        	bola.position.x+=-0.5;
+			        	bola.rotation.y+=0;
+		        	}
+		        break;
 
-	        case 38: // up
-	        break;
+		        case 38: // up
+		        break;
 
-	        case 39:
-	        	bola.rotation.y+=0.1;
-	        	bola.position.x+=0.5;
-	        	bola.rotation.y+=0;
-	        break;
+		        case 39:
+		        	if(bola.position.x < 125) {
+			        	bola.rotation.y+=0.1;
+			        	bola.position.x+=0.5;
+			        	bola.rotation.y+=0;
+		        	}
+		        break;
 
-	        case 40: // down
-	        break;
+		        case 40: // down
+		        break;
 
-	        default: return; // exit this handler for other keys
-	    }
+		        default: return; // exit this handler for other keys
+		    }
 	    e.preventDefault(); // prevent the default action (scroll / move caret)
 	});
+
+	//Função para caso a janela seja redimensionada
+	window.addEventListener('resize', function() {
+	    var WIDTH = window.innerWidth-60,
+	        HEIGHT = window.innerHeight;
+    	renderer.setSize(WIDTH, HEIGHT);
+    	camera.aspect = WIDTH / HEIGHT;
+    	camera.updateProjectionMatrix();
+    });
 
 }
 
